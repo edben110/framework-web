f132dc26e723c13524198b352f2555b844eb9e02
Nicki0m <nicolmunoz004@gmail.com>
2026-02-17 10:14:38 -0500

    feat: Agregando Botón

--- js.js
+++ js.js
@@ -1,13 +1,15 @@
-const prompt = require('prompt-sync')({ sigint: true });
-let word = prompt('ingrese la palabra o frase a contar vocales: ')
-let letters=['A','E','I','O','U']
-let vocals = 0
-for (let i = 0;i < word.length;i++){
-    for(let j =0;j < letters.length;j++){
-        if (word[i]==letters[j]||word[i]==letters[j].toLowerCase()){
-            vocals++
+function contarVocales() {
+    let word = prompt('Ingrese la palabra o frase a contar vocales:')
+    let letters = ['A','E','I','O','U']
+    let vocals = 0
+
+    for (let i = 0; i < word.length; i++) {
+        for (let j = 0; j < letters.length; j++) {
+            if (word[i] == letters[j] || word[i] == letters[j].toLowerCase()) {
+                vocals++
+            }
         }
     }
-}
 
-console.log('la frase o palabra (',word, ') tiene ', vocals,' vocales')
+    alert('La frase o palabra "' + word + '" tiene ' + vocals + ' vocales')
+}
